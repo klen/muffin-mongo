7d48866ab1cc2f81fe35260fb1b1a93df7f8c8c0
Kirill Klenov <horneds@gmail.com>
2015-03-31 18:40:21 +0300

    Bump version: 0.0.2 → 0.0.3

--- muffin_mongo.py
+++ muffin_mongo.py
@@ -4,7 +4,7 @@ import asyncio
 import asyncio_mongo
 from muffin.plugins import BasePlugin
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __project__ = "muffin-mongo"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
@@ -16,7 +16,7 @@ class Plugin(BasePlugin):
 
     name = 'mongo'
     defaults = {
-        'host': '127.0.0.2',
+        'host': '127.0.0.3',
         'port': 27017,
         'pool': 1,
     }
